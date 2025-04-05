0dc60a4bf59614ea98f51bff99a599edde15a6f6
samdouble <samleafs@protonmail.com>
2025-04-05 07:11:35 -0400

    feat: count words in sentences

--- dist/index.js
+++ dist/index.js
@@ -8,17 +8,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = run;
 const glob_1 = require("glob");
+const promises_1 = __importDefault(require("node:fs/promises"));
+const marked_1 = __importDefault(require("marked"));
 // import { getInput, setFailed } from '@actions/core';
+const config = [{
+        pattern: 'src/**/*.md',
+        rules: [{
+                "max-sentence-length": 25,
+            }]
+    }];
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        // const token = getInput('gh-token');
         // const label = getInput('label');
-        const markdownFiles = yield (0, glob_1.glob)('**/*.md', { ignore: 'node_modules/**' });
+        const markdownFiles = yield (0, glob_1.glob)('src/**/*.md', { ignore: 'node_modules/**' });
         for (const markdownFile of markdownFiles) {
+            const data = yield promises_1.default.readFile(markdownFile, { encoding: 'utf8' });
             console.log(markdownFile);
+            const tokens = marked_1.default.parse(data);
+            console.log(tokens);
+            const sentences = data.split('.');
+            for (const sentence of sentences) {
+                const trimmedSentence = sentence.trim();
+                const words = trimmedSentence.split(' ');
+                if (words.length > 25) {
+                    //console.log(sentence);
+                }
+            }
         }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import { glob } from 'glob';
+import fs from 'node:fs/promises';
+import marked from 'marked';
+
 // import { getInput, setFailed } from '@actions/core';
 
+const config = [{
+  pattern: 'src/**/*.md',
+  rules: [{
+    "max-sentence-length": 25,
+  }]
+}];
+
 export async function run() {
-  // const token = getInput('gh-token');
   // const label = getInput('label');
 
-  const markdownFiles = await glob('**/*.md', { ignore: 'node_modules/**' });
+  const markdownFiles = await glob('src/**/*.md', { ignore: 'node_modules/**' });
   for (const markdownFile of markdownFiles) {
+    const data = await fs.readFile(markdownFile, { encoding: 'utf8' });
     console.log(markdownFile);
+    const tokens = marked.parse(data);
+    console.log(tokens);
+    const sentences = data.split('.');
+    for (const sentence of sentences) {
+      const trimmedSentence = sentence.trim();
+      const words = trimmedSentence.split(' ');
+      if (words.length > 25) {
+        //console.log(sentence);
+      }
+    }
   }
 }
 
